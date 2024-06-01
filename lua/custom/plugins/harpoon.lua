f4c4189c8192d0bb206bfdef14f19f76a23b9d14
Stuart Dallas <stuart@stut.net>
2024-06-01 21:48:42 +0100

    Changed keymaps for harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,23 +1,22 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  config = function()
-    local harpoon = require "harpoon"
-    harpoon:setup()
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-    vim.keymap.set("n", "<m-h><m-m>", function()
-      harpoon:list():add()
-    end)
-    vim.keymap.set("n", "<m-h><m-l>", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>hl", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-    -- Set <space>1..<space>5 be my shortcuts to moving to the files
-    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
-      vim.keymap.set("n", string.format("<space>%d", idx), function()
-        harpoon:list():select(idx)
-      end)
-    end
-  end,
+		-- Set <space>1..<space>5 be my shortcuts to moving to the files
+		for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
+			vim.keymap.set("n", string.format("<space>%d", idx), function()
+				harpoon:list():select(idx)
+			end)
+		end
+	end,
 }
-
