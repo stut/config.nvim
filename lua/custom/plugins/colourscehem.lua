97e42625247d63bb2e0a874aa125ec1fe26b733e
Stuart Dallas <stuart@stut.net>
2024-06-13 18:57:36 +0100

    Added papercolor theme

--- lua/custom/plugins/colourscehem.lua
+++ lua/custom/plugins/colourscehem.lua
@@ -1,10 +1,14 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 4242,
-    config = function()
-      vim.cmd.colorscheme "catppuccin"
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 4242,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"NLKNguyen/papercolor-theme",
+		priority = 1000,
+	},
 }
