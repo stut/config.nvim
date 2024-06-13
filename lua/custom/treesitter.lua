b97e56f50ccaac42935c8ac4ba5630eab5fb0a3d
Stuart Dallas <stuart@stut.net>
2024-06-13 18:57:26 +0100

    Fixed treesitter

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -1,37 +1,9 @@
--- local treesitter = require "nvim-treesitter"
-
-local M = {}
-
-M.setup = function()
-	local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
-
-	require("nvim-treesitter").setup({
-		ensure_install = { "core", "stable" },
-		ensure_installed = { "c", "go", "lua", "vim", "vimdoc", "query", "javascript", "html" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-	})
-
-	vim.api.nvim_create_autocmd("FileType", {
-		group = group,
-		callback = function()
-			pcall(vim.treesitter.start)
-		end,
-	})
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "TSUpdate",
-		callback = function()
-			local parsers = require("nvim-treesitter.parsers")
-
-			parsers.lua = {
-				tier = 0,
-			}
-		end,
-	})
-end
-
-M.setup()
-
-return M
+require("nvim-treesitter.configs").setup({
+	modules = {},
+	auto_install = true,
+	ensure_installed = { "c", "lua", "go", "javascript", "html" },
+	ignore_install = {},
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },
+})
